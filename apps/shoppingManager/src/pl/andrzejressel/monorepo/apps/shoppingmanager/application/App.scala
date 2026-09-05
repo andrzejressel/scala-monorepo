a4a68a37433b2f2a1f06826debe3f15a63315472
Andrzej Ressel <github@andrzejressel.pl>
2026-09-05 23:24:14 +0200

    Add example app

---
+++ apps/shoppingManager/src/pl/andrzejressel/monorepo/apps/shoppingmanager/application/App.scala
@@ -0,0 +1,24 @@
+package pl.andrzejressel.monorepo.apps.shoppingmanager.application
+
+import scalafx.application.JFXApp3
+import scalafx.geometry.Pos
+import scalafx.scene.Scene
+import scalafx.scene.layout.VBox
+import scalafx.scene.text.Text
+
+object App extends JFXApp3:
+
+  override def start(): Unit =
+
+    stage = new JFXApp3.PrimaryStage:
+      title = "Shopping Manager"
+      width = 800
+      height = 600
+      scene = new Scene:
+        root = new VBox:
+          alignment = Pos.Center
+          spacing = 20
+          children = Seq(
+            new Text:
+              text = "Shopping Manager"
+          )
